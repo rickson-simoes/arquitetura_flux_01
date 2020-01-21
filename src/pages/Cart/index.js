cf314fe7bc01683e10da2f6c6123959c9adfa95d
Rickson <rickson.simoes@hotmail.com>
2020-01-21 15:47:57 -0300

    :hammer: Inicio do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Cart() {
-  return <div>Aeee</div>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th />
+          </tr>
+        </thead>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
